c88663d3fc7d0e91b9443f4b31462fbf7069a4f5
OlgaStarchenko <o9915530@gmail.com>
2026-04-12 15:58:42 +1000

    I created the app layout in a single AppLayout.jsx component. I defined all the necessary styles in a single app.module.css file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import "./App.css";
+import { AppLayout } from "./AppLayout";
 
 export function App() {
-	return <>Hello!</>;
+	return <AppLayout />;
 }
---
+++ src/AppLayout.jsx
@@ -0,0 +1,46 @@
+import styles from "./App.module.css";
+export function AppLayout() {
+	return (
+		<div className={styles.background}>
+			<div className={styles.container}>
+				<h1 className={styles.title}>To Do List for Girls</h1>
+				<div className={styles.input__container}>
+					<input className={styles.input} />
+					<button className={styles.add__btn}>Add</button>
+				</div>
+
+				<div className={styles.todo__list}>
+					<ul>
+						<div className={styles.todo__item}>
+							<input
+								type="checkbox"
+								className={styles.input__checkbox}
+							></input>
+							<li className={styles.item}>
+								Купить картошки, пожарить и съесть
+							</li>
+							<button className={styles.edit__btn}>/</button>
+							<button className={styles.delete__btn}>X</button>
+						</div>
+					</ul>
+				</div>
+
+				<div className={styles.filter__container}>
+					<div className={styles.filter__top}>
+						<button className={styles.filter__btn}>All</button>
+						<button className={styles.filter__btn}>Active</button>
+						<button className={styles.filter__btn}>
+							Completed
+						</button>
+					</div>
+
+					<div className={styles.filter__bottom}>
+						<button className={styles.filter__btn}>
+							Clear Completed
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
